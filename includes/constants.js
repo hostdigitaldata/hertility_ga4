e2d2c20d726123fd215d55350e66cc8161c3b3ce
hostdigitaldata <78739269+hostdigitaldata@users.noreply.github.com>
2023-09-05 15:57:39 +0300

    Initial commit

---
+++ includes/constants.js
@@ -0,0 +1,12 @@
+const GA4_DATABASE = 'hostinsight';
+const GA4_DATASET = 'analytics_358661708';
+const GA4_TABLE = 'events_*';
+const HOME_PAGE_REGEX = '^https:\/\/niococktails\.co\.uk\/?(\\?.*)?$';
+const BUILD_A_BOX_PAGE_REGEXP = '/collections/build-a-box'
+const COLLECTIONS_PAGE_REGEXP = '.*collections.*'
+const PRODUCTS_PAGE_REGEXP = '/products/'
+const BLOGS_PAGE_REGEXP = '/blogs/'
+const TIME_ZONE = 'Europe/London'
+
+
+module.exports = {GA4_DATABASE, GA4_DATASET, GA4_TABLE, HOME_PAGE_REGEX, TIME_ZONE }
