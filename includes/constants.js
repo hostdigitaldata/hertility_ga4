227258b4eebb955104fb09180a9420c09032b1e2
Alan Chapman <hostinsight@gmail.com>
2023-09-20 14:11:14 +0000

    funnel update

--- includes/constants.js
+++ includes/constants.js
@@ -1,12 +1,7 @@
-const GA4_DATABASE = 'hostinsight';
-const GA4_DATASET = 'analytics_358661708';
+const GA4_DATABASE = 'swift-hope-397007';
+const GA4_DATASET = 'analytics_283926911';
 const GA4_TABLE = 'events_*';
-const HOME_PAGE_REGEX = '^https:\/\/niococktails\.co\.uk\/?(\\?.*)?$';
-const BUILD_A_BOX_PAGE_REGEXP = '/collections/build-a-box'
-const COLLECTIONS_PAGE_REGEXP = '.*collections.*'
-const PRODUCTS_PAGE_REGEXP = '/products/'
-const BLOGS_PAGE_REGEXP = '/blogs/'
 const TIME_ZONE = 'Europe/London'
 
 
-module.exports = {GA4_DATABASE, GA4_DATASET, GA4_TABLE, HOME_PAGE_REGEX, TIME_ZONE }
+module.exports = {GA4_DATABASE, GA4_DATASET, GA4_TABLE, TIME_ZONE }
