6bd3015531d1a3160708e311b449263021547506
Alan Chapman <hostinsight@gmail.com>
2024-09-04 11:28:09 +0000

    .

--- includes/constants.js
+++ includes/constants.js
@@ -1,4 +1,4 @@
-const GA4_DATABASE = 'swift-hope-397007';
+const GA4_DATABASE = 'hertility-web-and-app-data';
 const GA4_DATASET = 'analytics_283926911';
 const GA4_TABLE = 'events_*';
 const TIME_ZONE = 'Europe/London'
